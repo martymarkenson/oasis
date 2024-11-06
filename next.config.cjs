e8aa1a3ea5a18f5261c4dc2b368e7560f67db3cc
Martin Markenson <martyamark@gmail.com>
2024-11-06 13:16:40 -0500

    Remove Sanity integration and clean up related files
    - Remove Sanity Studio and related configurations
    - Remove next-sanity dependency
    - Clean up duplicate next.config files
    - Remove unused Sanity schemas and client

--- next.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['eu-west-2.graphassets.com'], // Add your hostname here
-  },
-};
-
-module.exports = nextConfig; 
