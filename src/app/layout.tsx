b03e3a36eb64b20a9065f4e22c86085d962137f9
Martin Markenson <martyamark@gmail.com>
2024-11-06 17:56:05 -0500

    no more overscrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,10 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        {children}
+        <div className="overflow-y-auto h-screen">{children}</div>
       </body>
     </html>
   );
