ca597d692590e9b59c281271ed3572c7fc096fb8
Martin Markenson <martyamark@gmail.com>
2024-11-07 10:10:45 -0500

    changed animation setup so its smooth on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import MobileNav from './MobileNav';
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -39,18 +40,31 @@ const Header = () => {
         
         <div className="container mx-auto px-6">
           <div className="flex items-center justify-between py-4">
-            <a href="/" className="relative group">
+            <a
+              href="/"
+              className="relative group"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsAnimating(true);
+                setTimeout(() => {
+                  setIsAnimating(false);
+                }, 600);
+              }}
+            >
               <div className="flex items-center">
                 <div className="relative flex items-center">
-                  <img src="/images/gavel.png" alt="Gavel" className="w-10 h-10 mr-1" />                </div>
+                  <img src="/images/gavel.png" alt="Gavel" className="w-10 h-10 mr-1" />
+                </div>
                 <div className="relative">
                   <span className="text-2xl md:text-3xl font-semibold text-blue-600">vacations</span>
                   <span className="text-2xl md:text-3xl font-semibold text-gray-900">forauction</span>
                   <span className="text-2xl md:text-2xl font-semibold text-gray-900">.com</span>
                   {/* Animated underline */}
-                  <div className="absolute bottom-[-1px] left-[-1px] h-[2px] w-[2.3ch] bg-black
+                  <div
+                    className={`absolute bottom-[-1px] left-[-1px] h-[2px] w-[1.8ch] bg-black
                                 transition-all duration-300 ease-in-out
-                                group-hover:w-[32ch] rounded-full" />
+                                ${isAnimating ? 'w-[32ch]' : 'group-hover:w-[32ch]'} rounded-full`}
+                  />
                 </div>
               </div>
             </a>
