9a673357f114aff264e0c23847ac26b2b47e45d3
Martin Markenson <martyamark@gmail.com>
2024-11-07 19:11:21 -0500

    hopefully fixed deploy issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-import pkg from "next";
-const { NextConfig } = pkg;
-
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
